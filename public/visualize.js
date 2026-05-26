c555c0d6d7e7e9ae1b8789eac6cc4a526dda830b
abrar-amin <abraramin.school@gmail.com>
2026-05-26 17:49:21 -0400

    switch backend to HTTPS via Cloudflare Tunnel

--- public/visualize.js
+++ public/visualize.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = window.BACKEND_URL ?? "http://35.254.191.33:4003";
+const BACKEND_URL = window.BACKEND_URL ?? "https://java-tutor-api.abraramin.dev";
 
 let trace = [];
 let currentStep = 0;
